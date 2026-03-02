ac3a5ffcc7520bc976ce6a402ae975c6ce4d9652
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-03 08:31:21 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='edprompt',
+    version='0.0.1',
+    description='python shell prompt cli editor joke saver prompt bloody hell lmao 🤣🤣🤣',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/edprompt_env',
+    packages=find_packages(exclude=[]),
+    install_requires=['edprompt'],
+    keywords=['python', 'shell', 'prompt', 'cli', 'editer', 'joke' 'saver', 'edprompt', 'env', 'edpromptenv', 'edprompt_env'],
+    python_requires='>=3.13',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
